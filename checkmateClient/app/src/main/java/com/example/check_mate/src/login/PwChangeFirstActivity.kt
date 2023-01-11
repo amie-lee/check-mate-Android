1791a6bbe3e8e37abc5a359af44f00e4a9966bd5
GyuriShim <rabitskr@naver.com>
2023-01-12 00:33:56 +0900

    파일 생성

---
+++ checkmateClient/app/src/main/java/com/example/check_mate/src/login/PwChangeFirstActivity.kt
@@ -0,0 +1,13 @@
+package com.example.check_mate.src.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.check_mate.R
+import com.example.check_mate.config.BaseActivity
+import com.example.check_mate.databinding.ActivityPwChangeFirstBinding
+
+class PwChangeFirstActivity : BaseActivity<ActivityPwChangeFirstBinding>(ActivityPwChangeFirstBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ checkmateClient/app/src/main/java/com/example/check_mate/src/login/PwChangeSecondActivity.kt
@@ -0,0 +1,13 @@
+package com.example.check_mate.src.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.check_mate.R
+import com.example.check_mate.config.BaseActivity
+import com.example.check_mate.databinding.ActivityPwChangeSecondBinding
+
+class PwChangeSecondActivity : BaseActivity<ActivityPwChangeSecondBinding>(ActivityPwChangeSecondBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
