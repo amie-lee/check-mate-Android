14043af3c8c8d71e365cd5e45b064e4959572e79
minjgziii <kmj4668@gmail.com>
2023-01-16 19:23:40 +0900

    update 스플래시_시작화면

--- checkmateClient/app/src/main/java/com/example/check_mate/src/login/LoginActivity.kt
+++ checkmateClient/app/src/main/java/com/example/check_mate/src/login/LoginActivity.kt
@@ -1,7 +1,18 @@
 package com.example.check_mate.src.login
 
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.check_mate.R
 
-class LoginActivity {
+class LoginActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val button = findViewById<Button>(R.id.btn_login)
+        button.paintFlags = button.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+
+    }
 }
