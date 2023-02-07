81f53033f89c241ea43be059263b3c286ffef3a8
minjgziii <kmj4668@gmail.com>
2023-02-07 22:55:24 +0900

    알림 삭제 기능 추가

--- checkmateClient/app/src/main/java/com/example/check_mate/src/main/mate/NoticeActivity.kt
+++ checkmateClient/app/src/main/java/com/example/check_mate/src/main/mate/NoticeActivity.kt
@@ -1,37 +1,45 @@
 package com.example.check_mate.src.main.mate
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.check_mate.R
 import com.example.check_mate.config.BaseActivity
-import com.example.check_mate.databinding.ActivityMainBinding
 import com.example.check_mate.databinding.ActivityNoticeBinding
 
 class NoticeActivity: BaseActivity<ActivityNoticeBinding>(ActivityNoticeBinding::inflate){
 
     private lateinit var noticeBinding: ActivityNoticeBinding
 
-    val noticeData = arrayListOf<NoticeData>()
-    val NoticeAdapter = NoticeAdapter(noticeData)
+    private val data = arrayListOf<NoticeData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        noticeBinding = ActivityNoticeBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(noticeBinding.root)
+        setContentView(R.layout.activity_notice)
+        noticeBinding = ActivityNoticeBinding.inflate(layoutInflater)
+        val view = noticeBinding.root
+        setContentView(view)
 
+        noticeBinding.recycleNotice.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        noticeBinding.recycleNotice.adapter = NoticeAdapter(data,
+            onClickDeleteIcon = {
+                deleteTask(it)
+            }
+        )
+
+        // <- 버튼 누르면 mate 첫 화면으로 이동
         noticeBinding.noticeImgbtnBack.setOnClickListener {
             finish()
         }
 
-        noticeBinding.recycleNotice.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        noticeBinding.recycleNotice.adapter = NoticeAdapter
-
-
-        noticeData.add(NoticeData("메이트22", "님과 메이트가 되었습니다."))
-        noticeData.add(NoticeData("메이트11", "님이 수집품 룩을 잠금 해제했습니다."))
-        noticeData.add(NoticeData("메이트11", "님과 메이트가 되었습니다."))
+        data.add(NoticeData("메이트22", "님과 메이트가 되었습니다."))
+        data.add(NoticeData("메이트11", "님이 수집품 룩을 잠금 해제했습니다."))
+        data.add(NoticeData("메이트11", "님과 메이트가 되었습니다."))
     }
 
+
+    fun deleteTask(noticeData: NoticeData) {
+        data.remove(noticeData)
+        noticeBinding.recycleNotice.adapter?.notifyDataSetChanged()
+    }
 
 }
--- checkmateClient/app/src/main/java/com/example/check_mate/src/main/mate/NoticeAdapter.kt
+++ checkmateClient/app/src/main/java/com/example/check_mate/src/main/mate/NoticeAdapter.kt
@@ -6,25 +6,58 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.check_mate.R
+import com.example.check_mate.databinding.NoticeItemBinding
 
-class NoticeAdapter(val itemList: ArrayList<NoticeData>): RecyclerView.Adapter<NoticeAdapter.ViewHolder>() {
-    // (1) 아이템 레이아웃과 결합
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoticeAdapter.ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.notice_item, parent, false)
-        return ViewHolder(view)
+class NoticeAdapter(
+    private val itemList: List<NoticeData>,
+
+    val onClickDeleteIcon: (noticedata: NoticeData) -> Unit
+
+):
+    RecyclerView.Adapter<NoticeAdapter.NoticeViewHolder>() {
+
+
+    class NoticeViewHolder(val binding: NoticeItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+        val noticeItemMatename: TextView
+
+        get() {
+            TODO()
+        }
     }
+
+
+    // (1) 아이템 레이아웃과 결합
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): NoticeViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.notice_item, viewGroup, false)
+
+        return NoticeViewHolder(NoticeItemBinding.bind(view))
+    }
+
     // (2) 리스트 내 아이템 개수
     override fun getItemCount(): Int {
         return itemList.size
     }
+
     // (3) View에 내용 입력
-    override fun onBindViewHolder(holder: NoticeAdapter.ViewHolder, position: Int) {
-        holder.notice_matename.text = itemList[position].notice_matename
+    override fun onBindViewHolder(noticeViewHolder: NoticeViewHolder, position: Int) {
+        val listposition = itemList[position]
+        noticeViewHolder.binding.noticeItemMatename.text = listposition.notice_matename
+        noticeViewHolder.binding.noticeItemText.text = listposition.notice_text
+
+        noticeViewHolder.binding.noticeItemDeletebtn.setOnClickListener {
+            onClickDeleteIcon.invoke(listposition)
+        }
+
+        /*holder.notice_matename.text = itemList[position].notice_matename
         holder.notice_text.text = itemList[position].notice_text
+
+        holder.bind*/
     }
     // (4) 레이아웃 내 View 연결
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    /*class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val notice_matename: TextView = itemView.findViewById(R.id.notice_item_matename)
         val notice_text: TextView = itemView.findViewById(R.id.notice_item_text)
-    }
+    }*/
 }
