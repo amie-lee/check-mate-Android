a394879626c9608031a9baa1d87d12677865c99a
GyuriShim <rabitskr@naver.com>
2023-02-12 02:07:48 +0900

    투두 요약창 수정

--- checkmateClient/app/src/main/java/com/example/check_mate/src/main/checkboard/ResultTodoSumRVAdapter.kt
+++ checkmateClient/app/src/main/java/com/example/check_mate/src/main/checkboard/ResultTodoSumRVAdapter.kt
@@ -3,7 +3,10 @@ package com.example.check_mate.src.main.checkboard
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.marginEnd
 import androidx.recyclerview.widget.RecyclerView
+import com.example.check_mate.R
 import com.example.check_mate.databinding.ItemTodoBinding
 import com.example.check_mate.src.main.checkboard.models.ResultTodoSum
 
@@ -14,6 +17,12 @@ class ResultTodoSumRVAdapter(private val dataList: ArrayList<ResultTodoSum>): Re
             viewBinding.tvTodo.text = data.title
             viewBinding.tvDate.text = data.deadline
             viewBinding.tvMemo.text = data.memo
+            if (data.urgent) {
+                viewBinding.tvTagUrgent.setImageResource(R.drawable.ic_btn_urgent)
+            }
+            if (data.important) {
+                viewBinding.tvTagImportant.setImageResource(R.drawable.ic_btn_important)
+            }
         }
     }
 
--- checkmateClient/app/src/main/java/com/example/check_mate/src/main/checkboard/models/ResultTodoSum.kt
+++ checkmateClient/app/src/main/java/com/example/check_mate/src/main/checkboard/models/ResultTodoSum.kt
@@ -3,5 +3,7 @@ package com.example.check_mate.src.main.checkboard.models
 data class ResultTodoSum(
     val title: String,
     val deadline: String,
-    val memo: String
+    val memo: String,
+    val important: Boolean,
+    val urgent: Boolean
 )
